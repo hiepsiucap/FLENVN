071d2ece5352955cee758cd4b4b75098f12ac0b7
Bill H <billhiepnguyen@gmail.com>
2026-08-29 19:22:33 +0700

    Fix admin module JWT dependency

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AdminController } from './admin.controller';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_ACCESS_SECRET'),
+      }),
+    }),
+  ],
   controllers: [AdminController],
 })
 export class AdminModule {}
